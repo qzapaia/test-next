81f5f2a688375a8b7849ba19e68a21c805fbca55
Marcelo Zapaia <mzapaia@ints-MacBook-Pro.local>
2017-08-03 21:53:27 -0300

    com

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
-export default () => (
-  <div>Welcome to next.js!</div>
-)
+export default class extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      num:'server side rendering'
+    }
+
+  }
+  componentDidMount(){
+    this.interval = setInterval(()=>this.setState({ num:Math.random() }), 500);
+  }
+  componentWillUnmount(){
+    clearInterval(this.inverval)
+  }
+  render() {
+    return <h1>Random: {this.state.num}</h1>;
+  }
+}
